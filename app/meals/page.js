d328cf69f906d883389ca4f7b6d79d3a0655ae26
MartinMontagner <martinmontagner@estudiante.mdp.utn.edu.ar>
2025-09-13 09:53:42 -0300

    bbdd + loading

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,18 @@
+import { Suspense } from 'react';
 import classes from './page.module.css';
 import Link from 'next/link';
 import MealsGrid from '@/components/meals/meals-grid';
+import { getMeals } from '@/lib/meals';
+
+async function Meals() {
+        const meals = await getMeals();
+
+        return <MealsGrid meals={meals} />
+}
 
 export default function mealsPage(){
+
+
     return<>
         <header className={classes.header}>
             <h1>
@@ -19,7 +29,9 @@ export default function mealsPage(){
             </p>
         </header>
         <main className={classes.main}>
-            <MealsGrid meals={[]} />
+            <Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}>
+                <Meals />
+            </Suspense>
         </main>
     </>
 }
